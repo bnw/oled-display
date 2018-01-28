56c19c1b2209de0cc1f90f327bcc8f9228b4ef5a
Ben <benkl513@gmail.com>
2018-01-28 18:56:20 +0100

    Added npm stuff

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export {Pixel} from "./Pixel";
+export {Display} from "./Display";
+export {TextLine} from "./shape/TextLine";
+export {ImageFileCanvas} from "./ImageFileCanvas";
+export {OledLibCanvas} from "./OledLibCanvas";
